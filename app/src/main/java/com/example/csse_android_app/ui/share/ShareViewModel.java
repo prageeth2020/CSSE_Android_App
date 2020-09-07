2255f1524ec4cf20bf3e071f453d239244916759
prageeth2020 <prageethpramuditha.2020@gmail.com>
2020-09-08 02:10:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/csse_android_app/ui/share/ShareViewModel.java
@@ -0,0 +1,19 @@
+package com.example.csse_android_app.ui.share;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ShareViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ShareViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is share fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
