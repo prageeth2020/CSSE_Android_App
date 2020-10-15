5a9bcea2ab3e1b9fd209f82367012952352480f5
Piyumi14 <lbpmdayarathna@gmail.com>
2020-10-15 06:40:11 +0530

    Modifications in interfaces

---
+++ app/src/main/java/com/example/csse_android_app/DeliveryProcess2.java
@@ -0,0 +1,14 @@
+package com.example.csse_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliveryProcess2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_process2);
+    }
+}
