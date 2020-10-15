86743305a0a6fdbbcc10c9faeb7d613fa70abe11
Piyumi14 <lbpmdayarathna@gmail.com>
2020-10-15 06:51:45 +0530

    Created New Interface

---
+++ app/src/main/java/com/example/csse_android_app/PurchasingProcess3.java
@@ -0,0 +1,14 @@
+package com.example.csse_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PurchasingProcess3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchasing_process3);
+    }
+}
