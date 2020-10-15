c8796cbb4cbf6bca027814a046d6b6814e3d7a72
Piyumi14 <lbpmdayarathna@gmail.com>
2020-10-15 07:15:48 +0530

    Complted All Interfaces

---
+++ app/src/main/java/com/example/csse_android_app/EnquiryHandlingProcess2.java
@@ -0,0 +1,14 @@
+package com.example.csse_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnquiryHandlingProcess2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enquiry_handling_process2);
+    }
+}
